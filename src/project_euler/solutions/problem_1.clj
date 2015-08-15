5c5f39cc95e0f79acacd854b5099c09305e83f54
Jimmy Börjesson <lagginglion@gmail.com>
2015-08-15 12:55:00 +0200

    Solved problem 1

---
+++ src/project_euler/solutions/problem_1.clj
@@ -0,0 +1,16 @@
+(ns project-euler.solutions.problem-1)
+
+(def limit 1000)
+
+(def multiple-of (comp zero? mod))
+(defn condition
+  "Either a multiple of 3 or 5"
+  [n]
+  (or
+    (multiple-of n 3)
+    (multiple-of n 5)))
+
+(defn solution []
+  (reduce + (filter condition (range limit))))
+
+(time (solution))
