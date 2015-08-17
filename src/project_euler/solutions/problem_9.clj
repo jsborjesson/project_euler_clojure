2b02d4bdd24574ffb52d0d66e29f1726a941edb4
Jimmy Börjesson <lagginglion@gmail.com>
2015-08-17 19:20:27 +0200

    Solved problem 9

---
+++ src/project_euler/solutions/problem_9.clj
@@ -0,0 +1,22 @@
+(ns project-euler.solutions.problem-9)
+
+(def limit 1000)
+
+(def square #(* % %))
+
+(defn pythagorean-triplets
+  "List of pythagorean triplets where a + b + c = n"
+  [n]
+  (filter
+    (fn [[a b c]]
+      (= (+ (square a) (square b)) (square c)))
+    ; All combinations of 3 numbers where a < b < c and a + b + c = limit
+    (for [a (range n)
+          b (range n)
+          c (range n)
+          :when (and
+                  (< a b c)
+                  (= n (+ a b c)))]
+      [a b c])))
+
+(time (reduce * (first (pythagorean-triplets limit))))
