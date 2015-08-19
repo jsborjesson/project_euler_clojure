ac2c3517abd7ac101c78decbb409709d62f91f62
Jimmy Börjesson <lagginglion@gmail.com>
2015-08-19 14:16:27 +0200

    Problem 2: Much easier to grasp fib generation

--- src/project_euler/solutions/problem_2.clj
+++ src/project_euler/solutions/problem_2.clj
@@ -2,9 +2,11 @@
 
 (def limit 4000000)
 
-(def fib (lazy-cat '(0 1) (map + fib (rest fib))))
+(defn fib
+  ([] (fib 1 1))
+  ([a b] (cons a (lazy-seq (fib b (+ b a))))))
 
 (defn solution []
-  (reduce + (filter even? (take-while #(< % limit) fib))))
+  (reduce + (filter even? (take-while #(< % limit) (fib)))))
 
 (time (solution))
